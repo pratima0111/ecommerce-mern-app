dcbf14a9ccd25f6b592340e6ab31de5b608091ad
Pratima Singh <gk1137@myamu.ac.in>
2023-07-27 22:10:10 +0530

    add node version

--- client/src/pages/Admin/AdminOrders.js
+++ client/src/pages/Admin/AdminOrders.js
@@ -61,7 +61,7 @@ const AdminOrders = () => {
                         <th scope='col'>#</th>
                         <th scope='col'>Status</th>
                         <th scope='col'>Buyer</th>
-                        <th scope='col'> date</th>
+                        <th scope='col'>Date</th>
                         <th scope='col'>Payment</th>
                         <th scope='col'>Quantity</th>
                       </tr>
@@ -83,7 +83,7 @@ const AdminOrders = () => {
                           </Select>
                         </td>
                         <td>{o?.buyer?.name}</td>
-                        <td>{moment(o?.createAt).fromNow()}</td>
+                        <td>{moment(o?.createdAt).fromNow()}</td>
                         <td>{o?.payment.success ? 'Success' : 'Failed'}</td>
                         <td>{o?.products?.length}</td>
                       </tr>
